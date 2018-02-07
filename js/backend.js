177ee0e5ef06a49177f67aa862035eb062d4df6d
Pedro Alejandro Rodríguez Orellana <pedroaro123@gmail.com>
2018-02-06 21:37:24 -0400

    Post de ps4 y xbox agregados

--- js/backend.js
+++ js/backend.js
@@ -1,4 +1,14 @@
 $(function(){
 $('.grid').click(function(){location.href = 'publicacion.html';})
 $('.noti').click(function(){location.href = 'publicacion.html';})
+$('#ps4_pro').click(function(){location.href = 'post_ps4_pro.html';})
+$('#ps4_TLOU').click(function(){location.href = 'post_ps4_TLOU.html';})
+$('#ps4_VR').click(function(){location.href = 'post_ps4_VR.html';})
+$('#ps4_GOW').click(function(){location.href = 'post_ps4_GOW.html';})
+$('#ps4_10').click(function(){location.href = 'post_ps4_TOP10.html';})
+$('#xbox_one').click(function(){location.href = 'post_xbox.html';})
+$('#xbox_dayz').click(function(){location.href = 'post_xbox_dayz.html';})
+$('#xbox_gears').click(function(){location.href = 'post_xbox_gears.html';})
+$('#xbox_forza').click(function(){location.href = 'post_xbox_forza.html';})
+$('#xbox_halo').click(function(){location.href = 'post_xbox_halo.html';})
 });
